db6722e40e1853811aa49e7cec2ea1a9ae4e5089
dinopetrone <dinopetrone@gmail.com>
2014-08-20 07:29:04 -0700

    rich update

--- src/static/js/vendor/famous/rich/region.js
+++ src/static/js/vendor/famous/rich/region.js
@@ -77,10 +77,14 @@ Region = Region.extend({
     },
 
     open: function(view){
+        view.invalidateLayout();
         this.prepareSubviewAdd(view);
 
-        if(this.root){
-            this.invalidateLayout();
+        // this that have nestedSubviews
+        // will break if we don't ensure a context is present
+        // also, if no context is present, we have yet to be
+        // rendered anyway.
+        if(this.context){
             this.invalidateView();
         }
     },
